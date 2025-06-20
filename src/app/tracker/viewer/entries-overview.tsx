473bf008af243500b46a3181837f487a52849e13
Johnson <j.n.le@outlook.com>
2025-06-20 14:45:03 -0700

    entries overview

--- src/app/tracker/viewer/entries-overview.tsx
+++ src/app/tracker/viewer/entries-overview.tsx
@@ -1,8 +1,87 @@
-import { Card, CardContent  } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { SessionEntryProp } from "@/lib/drizzle/schema";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button";
+
+const AccordionItemWrapper = ({ value, trigger, content }: { value: string, trigger: React.ReactNode, content: React.ReactNode }) => {
+    return (
+        <Card className="px-2 py-0">
+            <AccordionItem value={value} className="">
+                <AccordionTrigger>
+                    {trigger}
+                </AccordionTrigger>
+                <AccordionContent>
+                    {content}
+                </AccordionContent>
+            </AccordionItem>
+        </Card>
+    )
+}
 
 
-const EntriesOverview = ({ entries }: {entries:SessionEntryProp[]}) => {
+const EntriesOverview = ({ entries }: { entries: SessionEntryProp[] }) => {
+    return (
+        <div className="space-y-4">
+            <h2 className="text-xl font-semibold">Session Entries</h2>
+
+            <Accordion
+                type="single"
+                collapsible
+                className="w-full"
+            >
+                <div className="space-y-2">
+                    {
+                        entries.map((entry) => (
+                            <AccordionItemWrapper
+                                key={entry.id}
+                                value={entry.id}
+                                trigger={
+                                    <div className="px-2">
+                                        <div className="text-2xl">{entry.stationName}</div>
+                                        <div className="text-xs font-normal">{entry.datetimeStart.toLocaleString()}</div>
+                                    </div>
+                                }
+                                content={
+                                    <div className="w-full border-t-[1px] pt-2 px-2">
+                                        <p><strong>Station:</strong> {entry.stationName}</p>
+                                        <p><strong>Start:</strong> {new Date(entry.datetimeStart).toLocaleString()}</p>
+                                        <p><strong>End:</strong> {new Date(entry.datetimeEnd).toLocaleString()}</p>
+                                        <p><strong>Fabricated Qty:</strong> {entry.fabricatedQty}</p>
+                                        {
+                                            (entry.issueDuration>0 &&
+                                                <>
+                                                    <br />
+                                                    <p><strong>Stop Duration:</strong> {entry.issueDuration} min</p>
+                                                    <p><strong>Stop Notes:</strong> {entry.issueNotes}</p>
+                                                </>
+                                            )
+                                        }
+                                        {
+                                            (entry.defectQty>0 &&
+                                                <>
+                                                    <br />
+                                                    <p><strong>Defects Qty:</strong> {entry.defectQty}</p>
+                                                    <p><strong>Defect Notes:</strong> {entry.defectNotes}</p>
+                                                </>
+                                            )
+                                        }
+                                        <Button className="w-full mt-4">Edit</Button>
+                                    </div>
+                                }
+                            />
+                        ))
+                    }
+                </div>
+
+            </Accordion>
+        </div>
+    )
+
     return (
         <div className="flex flex-col gap-4">
             <h2 className="text-xl font-semibold">Session Entries</h2>
